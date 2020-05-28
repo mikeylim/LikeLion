9bf2adf53caa5c6a0d6f6018dae3ff782ddf5870
mikeylim <mikey3437@gmail.com>
2020-05-28 03:36:48 -0700

    Cleaned up gibberish code for capital-game.rb

--- CodeLion/ruby/projects/capital-game.rb
+++ CodeLion/ruby/projects/capital-game.rb
@@ -4,25 +4,26 @@ pairs = {"한국"=>"서울","베트남"=>"하노이","미국"=>"워싱턴"}
 loop do
     # pairs의 사이즈가 0이 아니면 퀴즈를 계속해라!
     if pairs.size == 0
-        puts "잘하셨어요!!!!!!!!!! 끝입니다"
+        puts "문제를 모두 맞추셨습니다. 열심히 공부 하셨군요~ ^_^"
         break
     else
         pairs.each do |country, capital|
             puts "#{country} 의 수도를 맞추세요 : "
             answer = gets.chomp
             if (answer == capital)
-                puts "정 답 ----- #{country} 의 수도는 #{capital} 가 맞습니다."
+                puts "정 답 입니다!"
                 pairs.delete(country)
             else
-                puts "땡! 틀렸습니다. #{country} 의 수도는 #{capital} 입니다."
+                puts "땡! 틀렸습니다. #{country} 의 수도는 [#{capital}] 입니다."
             end
         end
+        puts "모든 질문이 끝났습니다. 오답을 묶어서 다시 시작합니다."
     end
 end
 
 # ###### 2nd way
 # pairs = {"한국" => "서울", "베트남" => "하노이", "미국" => "워싱턴"}
-# # 무한루프
+# # while loop
 # loop do
 #     pairs.each do |country, capital|
 #         puts "#{country} 의 수도를 맞추세요 : "
